12854dcb89bdbab915cff5f3b45a0da546787f71
MitsuSarkar <sumitsarkar2222@gmail.com>
2024-01-06 16:34:04 +0000

    syncfusion-dashbaord

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Category, Title } from '@syncfusion/ej2-react-charts'
+import React from 'react'
+
+const Header = ({ Category, title }) => {
+  return (
+    <div className="mb-10">
+    <p className="text-gray-400">
+    {Category}
+    </p>
+    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+    {title} 
+    </p>'
+    </div>
+  )
+}
+
+export default Header
